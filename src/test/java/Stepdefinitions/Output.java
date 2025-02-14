2e7349b909be6d9f1549faa8d47b9ecde53543b3
naveenr9348398 <naveen.rajasekaran74@gmail.com>
2025-02-14 12:50:40 +0530

    Ecworkspace2

--- src/test/java/Stepdefinitions/Output.java
+++ src/test/java/Stepdefinitions/Output.java
@@ -5,6 +5,8 @@ public class Output {
 	public static void main(String[] args) {
 		System.out.println("Output 1");
 		System.out.println("output2");
+		System.out.println("output3");
+		System.out.println("output4");
 	}
 
 }
